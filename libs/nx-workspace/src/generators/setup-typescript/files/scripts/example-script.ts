3570b7c971b723843fa1b988db345f167bedec5e
Jake Ginnivan <jake@ginnivan.net>
2022-07-09 13:44:14 +0800

    Removed broken reference

--- libs/nx-workspace/src/generators/setup-typescript/files/scripts/example-script.ts
+++ libs/nx-workspace/src/generators/setup-typescript/files/scripts/example-script.ts
@@ -3,15 +3,11 @@ import {} from 'node:path'
 ;(async function () {
     await removePublishDir('dist/libs/nx-terraform/package.json')
     await removePublishDir('dist/libs/nx-workspace/package.json')
-    await removePublishDir('dist/libs/nx-gh-actions/package.json')
 })()
 
 async function removePublishDir(nxTerraformPackageJsonName: string) {
     const packageJson = await readFile(nxTerraformPackageJsonName)
     const packageJsonParsed = JSON.parse(packageJson.toString())
     delete packageJsonParsed.publishConfig.directory
-    await writeFile(
-        nxTerraformPackageJsonName,
-        JSON.stringify(packageJsonParsed, null, 4)
-    )
+    await writeFile(nxTerraformPackageJsonName, JSON.stringify(packageJsonParsed, null, 4))
 }
