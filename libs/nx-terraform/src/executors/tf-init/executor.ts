12b84d670d4380f803e748cafd97669429d203d6
Jake Ginnivan <jake@ginnivan.net>
2022-08-29 11:30:10 +0800

    Need to be able to run init with an environment for migrating state

--- libs/nx-terraform/src/executors/tf-init/executor.ts
+++ libs/nx-terraform/src/executors/tf-init/executor.ts
@@ -1,13 +1,20 @@
 import { ExecutorContext } from '@nrwl/devkit'
+import publicIp from 'public-ip'
+import { addFirewallRules } from '../../common/addFirewallRules'
 import { ensureNoBackendFile } from '../../common/ensure-no-backend-file'
 import { initEnvironmentWorkspaceWithFirewallRuleRetry } from '../../common/initEnvironmentWorkspaceWithRetry'
+import { readRepoSettings } from '../../common/read-repo-settings'
+import { readConfigFromEnvFile } from '../../common/readConfigFromEnvFile'
+import { removeFirewallRules } from '../../common/removeFirewallRules'
 import { InitExecutorSchema } from './schema'
 
 export default async function runExecutor(options: InitExecutorSchema, context: ExecutorContext) {
+    const publicIpv4 = await publicIp.v4()
     if (!context.projectName) {
         throw new Error('No projectName')
     }
 
+    const repoConfig = readRepoSettings()
     const projectRoot = context.workspace.projects[context.projectName]?.root
 
     if (!projectRoot) {
@@ -16,19 +23,67 @@ export default async function runExecutor(options: InitExecutorSchema, context: 
             success: false,
         }
     }
-    ensureNoBackendFile(projectRoot)
 
-    await initEnvironmentWorkspaceWithFirewallRuleRetry({
-        terragruntConfigFile: 'vars/local/terragrunt.hcl',
-        terragruntCliArgs: [],
-        projectRoot,
-        upgrade: options.upgrade,
-        migrateState: options.migrateState,
-        reconfigure: options.reconfigure,
-        // These are not used because we will not hit firewall issues with local
-        retryAttempts: 3,
-        retryDelay: 5,
-    })
+    if (!options.environment) {
+        ensureNoBackendFile(projectRoot)
+
+        await initEnvironmentWorkspaceWithFirewallRuleRetry({
+            terragruntConfigFile: 'vars/local/terragrunt.hcl',
+            terragruntCliArgs: [],
+            projectRoot,
+            upgrade: options.upgrade,
+            migrateState: options.migrateState,
+            reconfigure: options.reconfigure,
+            // These are not used because we will not hit firewall issues with local
+            retryAttempts: 3,
+            retryDelay: 5,
+        })
+    } else {
+        const config = await readConfigFromEnvFile(
+            repoConfig.terraformStateType,
+            options.environment,
+        )
+        if (!config) {
+            console.warn('Skipped apply, no terragrunt file for environment')
+            return {
+                success: true,
+            }
+        }
+        const { resourceGroupName, terraformStorageAccount, terragruntConfigFile } = config
+
+        const { keyVaultsToRemoveFirewallRules, storageAccountsToRemoveFirewallRules } =
+            await addFirewallRules({
+                resourceGroupName,
+                addIpToKeyVaults: [],
+                addIpToStorageAccounts:
+                    options.addIpToDefaultStorage && terraformStorageAccount
+                        ? [terraformStorageAccount]
+                        : [],
+                publicIpv4,
+                terragruntConfigFile,
+                projectRoot,
+            })
+
+        try {
+            await initEnvironmentWorkspaceWithFirewallRuleRetry({
+                terragruntConfigFile,
+                terragruntCliArgs: [],
+                projectRoot,
+                upgrade: options.upgrade,
+                migrateState: options.migrateState,
+                reconfigure: options.reconfigure,
+                retryAttempts: options.firewallRetryAttempts,
+                retryDelay: options.firewallRetryDelay,
+            })
+        } finally {
+            await removeFirewallRules({
+                resourceGroupName,
+                removeIpFromKeyVaults: keyVaultsToRemoveFirewallRules,
+                removeIpFromStorageAccounts: storageAccountsToRemoveFirewallRules,
+                publicIpv4,
+            })
+        }
+    }
 
     return {
         success: true,
--- libs/nx-terraform/src/executors/tf-init/schema.d.ts
+++ libs/nx-terraform/src/executors/tf-init/schema.d.ts
@@ -2,4 +2,8 @@ export interface InitExecutorSchema {
     upgrade?: boolean
     reconfigure?: boolean
     migrateState?: boolean
+    environment?: string
+    addIpToDefaultStorage: boolean
+    firewallRetryAttempts: number
+    firewallRetryDelay: number
 }
