5936f82af6ce879ba98d91fe4b893f9f92c0b11c
Jake Ginnivan <jake@ginnivan.net>
2022-09-09 14:46:40 +0800

    Fixed location shortname

--- libs/nx-terraform/src/common/getEnvTfVars.ts
+++ libs/nx-terraform/src/common/getEnvTfVars.ts
@@ -24,7 +24,75 @@ export function getTfEnvVars(projectName: string, envConfig: EnvConfig, repoConf
 function getShortResourceLocation(resourceLocation: string) {
     // Taken from https://github.com/claranet/terraform-azurerm-regions/blob/master/regions.tf#L71
 
-    const short_names: Record<string, string> = {
+    const cliNames = {
+        'us-east': 'eastus', // East US
+        'us-east-2': 'eastus2', // East US 2
+        'us-south-central': 'southcentralus', // South Central US
+        'us-west-2': 'westus2', // West US 2
+        'asia-south-east': 'southeastasia', // Southeast Asia
+        'eu-north': 'northeurope', // North Europe
+        'uk-south': 'uksouth', // UK South
+        'eu-west': 'westeurope', // West Europe
+        'us-central': 'centralus', // Central US
+        'us-north-central': 'northcentralus', // North Central US
+        'us-west': 'westus', // West US
+        'saf-north': 'southafricanorth', // South Africa North
+        'ind-central': 'centralindia', // Central India
+        'asia-east': 'eastasia', // East Asia
+        'jap-east': 'japaneast', // Japan East
+        'kor-central': 'koreacentral', // Korea Central
+        'can-central': 'canadacentral', // Canada Central
+        'fr-central': 'francecentral', // France Central
+        'ger-west-central': 'germanywestcentral', // Germany West Central
+        'norw-east': 'norwayeast', // Norway East
+        'swz-north': 'switzerlandnorth', // Switzerland North
+        'uae-north': 'uaenorth', // UAE North
+        'bra-south': 'brazilsouth', // Brazil South
+        asia: 'asia', // Asia
+        'asia-pa': 'asiapacific', // Asia Pacific
+        aus: 'australia', // Australia
+        bra: 'brazil', // Brazil
+        can: 'canada', // Canada
+        eu: 'europe', // Europe
+        global: 'global', // Global
+        ind: 'india', // India
+        jap: 'japan', // Japan
+        uk: 'uk', // United Kingdom
+        us: 'unitedstates', // United States
+        'us-west-central': 'westcentralus', // West Central US
+        'saf-west': 'southafricawest', // South Africa West
+        'aus-central': 'australiacentral', // Australia Central
+        'aus-central-2': 'australiacentral2', // Australia Central 2
+        'aus-east': 'australiaeast', // Australia East
+        'aus-south-east': 'australiasoutheast', // Australia Southeast
+        'jap-west': 'japanwest', // Japan West
+        'kor-south': 'koreasouth', // Korea South
+        'ind-south': 'southindia', // South India
+        'ind-west': 'westindia', // West India
+        'can-east': 'canadaeast', // Canada East
+        'fr-south': 'francesouth', // France South
+        'ger-north': 'germanynorth', // Germany North
+        'norw-west': 'norwaywest', // Norway West
+        'swz-west': 'switzerlandwest', // Switzerland West
+        'uk-west': 'ukwest', // UK West
+        'uae-central': 'uaecentral', // UAE Central
+        'bra-south-east': 'brazilsoutheast', // Brazil Southeast
+        'ger-north-east': 'germanynortheast', // "Germany Northeast"
+        'ger-central': 'germanycentral', // "Germany Central"
+
+        'cn-north': 'chinanorth', // "China North"
+        'cn-east': 'chinaeast', // "China East"
+        'cn-east-2': 'chinaeast2', // "China East 2"
+        'cn-north-2': 'chinanorth2', // "China North 2"
+        'cn-east-3': 'chinaeast3', // "China East 3"
+        'cn-north-3': 'chinanorth3', // "China North 3"
+    }
+    // Reverse cliNames key and values
+    const cliNameLookup = Object.entries(cliNames)
+        .map(([key, value]) => [value, key])
+        .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
+
+    const short_names = {
         'us-east': 'ue',
         'us-east-2': 'ue2',
         'us-central': 'uc',
@@ -89,5 +157,5 @@ function getShortResourceLocation(resourceLocation: string) {
         us: 'us', // United States
     }
 
-    return short_names[resourceLocation] || resourceLocation
+    return short_names[cliNameLookup[resourceLocation]] || resourceLocation
 }
