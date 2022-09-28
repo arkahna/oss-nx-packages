f9a1201c63b047f0f076578c0595a834788ba21a
Jake Ginnivan <jake@ginnivan.net>
2022-09-28 20:09:05 +0800

    Cleaned up unneeded params on state executor

--- libs/nx-terraform/src/executors/state/schema.d.ts
+++ libs/nx-terraform/src/executors/state/schema.d.ts
@@ -2,10 +2,7 @@ export interface StateExecutorSchema {
     command: string
     address?: string
     environment: string
-    addIpToDefaultKeyVault: boolean
     addIpToDefaultStorage: boolean
-    addIpToKeyVaults?: string[]
-    addIpToStorage?: string[]
     firewallRetryAttempts: number
     firewallRetryDelay: number
     json: boolean
